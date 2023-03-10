7cc9f9c70c65fe9413958a807fe56e3690954ef9
DESKTOP-RFQ9V3C\take <goozooloo123@gmail.com>
2023-03-10 11:55:45 +0900

    system-spec実行

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,4 +64,15 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include FactoryBot::Syntax::Methods
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
+    end
+  end
 end
